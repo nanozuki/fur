774c508775f80a03e5901026be8cccdf371c4914
Nanozuki Crows <nanozuki.crows@gmail.com>
2021-08-25 23:01:39 +0800

    start to make sence

--- testground/main.ts
+++ testground/main.ts
@@ -1,10 +1,30 @@
-import { Brew } from "../regulators/brew.ts";
-import { DotFile } from "../regulators/dotfile.ts";
 import { Feature } from "../feature.ts";
 import { Set } from "../set.ts";
 import { Nvix } from "../nvix.ts";
+import { Brew } from "../regulators/brew.ts";
+import { DotFile } from "../regulators/dotfile.ts";
+import { Command } from "../util/command.ts";
 
-async function main() {
+async function macos() {
+  const basic = new Set(
+    new Brew("make", "tree", "direnv"), // TODO: cask
+    new Command(
+      "fish",
+      "-c",
+      "set PATH ~/.local/bin /opt/homebrew/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin /Library/Apple/usr/bin",
+    ), // TODO: move to fish set.
+  );
+  const fish = new Set(
+    new Brew("fish"),
+  );
+  const git = new Set();
+  const tmux = new Set();
+  const nvim = new Set();
+  const go = new Feature("go");
+  const rust = new Feature("rust");
+  const zig = new Feature("zig");
+
+  /*
   const rust = new Feature("rust")
     .useRegulators(
       new Brew("rustup", "rust-analyzer"),
@@ -18,8 +38,9 @@ async function main() {
     new Brew("tmux"),
     new DotFile("./templates/tmux", "~/.config/tmux", {}),
   );
-  const neovix = new Nvix(tmux, rust);
+  */
+  const neovix = new Nvix(basic, fish, git, tmux, nvim, go, rust, zig);
   await neovix.exec();
 }
 
-await main();
+await macos();
